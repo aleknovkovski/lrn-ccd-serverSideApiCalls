7683550a16ab3e3aee8bd0c99ef45643c8c153cf
Alek Novkovski <alek@greatcode2.com>
2023-01-28 19:55:00 +0100

    Setup createClient, export

--- working/backend/provider/supabase.js
+++ working/backend/provider/supabase.js
@@ -6,9 +6,11 @@ require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
 // Create two const variables `supabaseURL` and `supabaseSecret` with the value of the environment variables you created.
-
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseSecret = process.env.SUPABASE_SECRET;
 
 // Create a variable `supabase` and set the value to the execution of the `createClient` function passing in `supabaseUrl` and `supabaseSecret` variables as arguments. 
+const supabase = createClient(supabaseUrl, supabaseSecret);
 
-
-// Export the `supabase` variable. 
+// Export the `supabase` variable.
+module.exports = supabase;
